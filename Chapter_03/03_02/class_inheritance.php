d0769be86d339b7347741fb632711616b7cd6fc9
Bret <Burhooo@gmail.com>
2018-12-12 20:54:01 -0600

    finished ch3

--- Chapter_03/03_02/class_inheritance.php
+++ Chapter_03/03_02/class_inheritance.php
@@ -15,4 +15,28 @@ class User {
 class Customer extends User {
 }
 
+$u = new User;
+$u->first_name = 'Jerry';
+$u->last_name = 'Seinfeld';
+$u->username = 'jseinfeld';
+
+$c = new Customer;
+$c->first_name = 'George';
+$c->last_name = 'Costanza';
+$c->username = 'gcostanza';
+
+echo $u->full_name() . '<br />';
+echo $c->full_name() . '<br />';
+
+echo get_parent_class($u) . '<br />';
+echo get_parent_class($c) . '<br />';
+
+
+if(is_subclass_of($c, 'User')) {
+  echo "Instance is a subclass of User.<br />";
+}
+
+$parents = class_parents($c);
+echo implode(', ', $parents) . '<br />';
+
 ?>
--- Chapter_03/03_03/oop_sandbox/class_inheritance.php
+++ Chapter_03/03_03/oop_sandbox/class_inheritance.php
@@ -13,7 +13,25 @@ class User {
 }
 
 class Customer extends User {
+
+  var $city;
+  var $state;
+  var $country;
+  var $is_admin = false;
+
+  function location() {
+    return $this->city . ", " . $this->state . ", " .$this->country;
+  }
 }
+
+class AdminUser extends User {
+  var $is_admin = true;
+
+  function full_name() {
+    return $this->first_name . " " . $this->last_name . '(Admin)';
+  }
+}
+
 
 $u = new User;
 $u->first_name = 'Jerry';
@@ -24,9 +42,14 @@ $c = new Customer;
 $c->first_name = 'George';
 $c->last_name = 'Costanza';
 $c->username = 'gcostanza';
+$c->city = 'New York';
+$c->state = 'New York';
+$c->country = 'United States';
 
 echo $u->full_name() . '<br />';
 echo $c->full_name() . '<br />';
+
+echo $c->location() . '<br />';
 
 echo get_parent_class($u) . '<br />';
 echo get_parent_class($c) . '<br />';
